aeb63ce7e4a2a3a4b5c0b156de5e05418169c462
hothandto17 <hothandto17@gmail.com>
2024-08-09 01:56:18 +0700

    MISA

--- MISA.Web2024/MISA.Web2024.Api/Model/Department.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MISA.Web2024.Api.Model
-{
-    public class Department
-    {
-        public Guid DepartmentID { get; set; }
-        public string DepartmentName { get; set; }
-        public string DepartmentCode { get; set; }
-    }
-}
--- MISA.Web2024/MISA.Web2024.Api/Model/Position.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MISA.Web2024.Api.Model
-{
-    public class Position
-    {
-        public Guid PositionId { get; set; }
-        public string PositionName { get; set; }
-        public string PositionCode { get; set; }
-    }
-}
